40c86e15cde1dc204f24a716e5c6686e2c30fd16
unknown <nilton.formis@gmail.com>
2022-04-09 17:09:40 -0300

    fix run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,5 +1,5 @@
 delorean="robot -l NONE -o NONE -r NONE tasks/Delorean.robot"
-teste="robot -x xunit.xml -d ./logs/tests ./tests"
+teste="robot -x xunit.xml -d ./logs"
 
 echo "Construindo ambiente:"
 $delorean
